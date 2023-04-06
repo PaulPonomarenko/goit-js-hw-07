e56f734290ac07ba2c0b98a3f6fb07447db7c127
PaulPonomarenko <pashaponomarenkoa1999@gmail.com>
2023-04-07 02:34:43 +0300

    добавляет 2е задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const mainGalleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+
+function createGallery(image) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
+mainGalleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+mainGalleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery__link", {
+    captionSelector: "img",
+    captionsData: "alt",
+    captionDelay: 250,
+    enableKeyboard: true,
+  });
+});
